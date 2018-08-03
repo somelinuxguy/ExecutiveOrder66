b52c2e4e9a05f9fefcdb5c98e5dabc7efa2311f7
Ian Gornall <ian.gornall@gmail.com>
2018-08-03 10:11:00 -0400

    Changed to full size images.  Fixed query variable.  Excluded facebook from results.

--- google image-search/main.js
+++ google image-search/main.js
@@ -1,9 +1,10 @@
 var displayData = function(data) {
+  console.log(data);
   var listItemHolder = document.createDocumentFragment();
   for (item of data.items){
     var itemDisplay = document.createElement('li');
     var imageDisplay = document.createElement('img');
-    imageDisplay.setAttribute('src', item.image.thumbnailLink);
+    imageDisplay.setAttribute('src', item.link);
     imageDisplay.setAttribute('alt', item.snippet);
     itemDisplay.appendChild(imageDisplay);
     var imageCaption = document.createElement('p');
@@ -13,7 +14,7 @@ var displayData = function(data) {
   document.querySelector('.image-list').appendChild(listItemHolder);
 }
 var search = function(query) {
-  $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=AIzaSyA_kqsDITB2TOWu5cVEY4Qi2v_4gizd4nE&searchType=image&q=' + query, displayData);
+  $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=AIzaSyA_kqsDITB2TOWu5cVEY4Qi2v_4gizd4nE&searchType=image&num=1&siteSearch=*.facebook.com/*&siteSearchFilter=e&q=' + query, displayData);
 }
 var searchForm = document.querySelector('.search-form');
 console.log(searchForm);
@@ -21,6 +22,7 @@ console.log(searchForm);
 searchForm.addEventListener('submit', function(event) {
   console.log('event logged');
   event.preventDefault();
-  var query = document.querySelector('[name="search"]')
+  var searchInput = document.querySelector('[name="search"]')
+  var query = searchInput.value;
   search(query);
 })
