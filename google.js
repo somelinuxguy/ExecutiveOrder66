15db41ddcb1f02136919d38ba87f2e30d8dc0be8
Ian Gornall <ian.gornall@gmail.com>
2018-08-03 14:26:46 -0400

    integrated google functions

---
+++ google.js
@@ -0,0 +1,5 @@
+var getImgSrc = function(query, callback) {
+  $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=' + googleAPIKey + '&searchType=image&num=1&siteSearch=*.facebook.com/*&siteSearchFilter=e&q=' + query, data => callback(data.items[0].link));
+}
+
+var getLocation = 'https://www.google.com/maps/embed/v1/place?key=' + googleCloudAPIKey + '&q=' + query + ',Atlanta+GA';
