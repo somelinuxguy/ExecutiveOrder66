41ccc4627d8b611906690fb361e0d42117d5dac3
Ian Gornall <ian.gornall@gmail.com>
2018-08-06 14:31:41 -0400

    Google is Safe

--- google.js
+++ google.js
@@ -1,5 +1,5 @@
 var getImgSrc = function(query, callback) {
-  $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=' + googleAPIKey + '&searchType=image&num=1&siteSearch=*.facebook.com/*&siteSearchFilter=e&q=' + query, data => callback(data.items[0].link));
+  $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=' + googleAPIKey + '&searchType=image&num=1&siteSearch=*.facebook.com/*&siteSearchFilter=e&safe=medium&q=' + query, data => callback(data.items[0].link));
 }
 
 var getLocation = function(query) {
