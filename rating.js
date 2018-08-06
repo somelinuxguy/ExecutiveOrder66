cf7ae5ea9772edaacb19b7b2b0d5d0a680f009f2
Matt <marberrym@gmail.com>
2018-08-06 10:21:52 -0400

    Added a rating function, still needs to be integrated to main JS files.  Also did minor graphic work

---
+++ rating.js
@@ -0,0 +1,52 @@
+var gem1 = document.querySelector("[name='gem1']");
+var gem2 = document.querySelector("[name='gem2']");
+var gem3 = document.querySelector("[name='gem3']");
+var gem4 = document.querySelector("[name='gem4']");
+var gem5 = document.querySelector("[name='gem5']");
+var rating = document.querySelector(".ratinglabel")
+
+ratingValue = 0;
+
+
+var rate = function(event) {
+    if (event.target === gem1) {
+        gem1.setAttribute('src', 'Images/gem3.png');
+        gem2.setAttribute('src', 'Images/unclickedgem.png')
+        gem3.setAttribute('src', 'Images/unclickedgem.png')
+        gem4.setAttribute('src', 'Images/unclickedgem.png')
+        gem5.setAttribute('src', 'Images/unclickedgem.png')
+        ratingValue = '1';
+    } else if (event.target === gem2) {
+        gem1.setAttribute('src', 'Images/gem3.png');
+        gem2.setAttribute('src', 'Images/gem3.png');
+        gem3.setAttribute('src', 'Images/unclickedgem.png')
+        gem4.setAttribute('src', 'Images/unclickedgem.png')
+        gem5.setAttribute('src', 'Images/unclickedgem.png')
+        ratingValue = '2';
+    } else if (event.target === gem3) {
+        gem1.setAttribute('src', 'Images/gem3.png');
+        gem2.setAttribute('src', 'Images/gem3.png');
+        gem3.setAttribute('src', 'Images/gem3.png');
+        gem4.setAttribute('src', 'Images/unclickedgem.png')
+        gem5.setAttribute('src', 'Images/unclickedgem.png')
+        ratingValue = '3';
+    } else if (event.target === gem4) {
+        gem1.setAttribute('src', 'Images/gem3.png');
+        gem2.setAttribute('src', 'Images/gem3.png');
+        gem3.setAttribute('src', 'Images/gem3.png');
+        gem4.setAttribute('src', 'Images/gem3.png');
+        gem5.setAttribute('src', 'Images/unclickedgem.png')
+        ratingValue = '4';
+    } else if (event.target === gem5) {
+        gem1.setAttribute('src', 'Images/gem3.png');
+        gem2.setAttribute('src', 'Images/gem3.png');
+        gem3.setAttribute('src', 'Images/gem3.png');
+        gem4.setAttribute('src', 'Images/gem3.png');
+        gem5.setAttribute('src', 'Images/gem3.png');
+        ratingValue = '5';
+    }
+    return ratingValue;
+}
+
+rating.addEventListener('click', rate);
+
