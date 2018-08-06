bd0730eeb5c521c2edb7daf1f20ad74ff154a9d1
Ian Gornall <ian.gornall@gmail.com>
2018-08-06 14:44:53 -0400

    Added filter to query only alphanumeric input with spaces or underscores

--- google.js
+++ google.js
@@ -1,5 +1,10 @@
 var getImgSrc = function(query, callback) {
-  $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=' + googleAPIKey + '&searchType=image&num=1&siteSearch=*.facebook.com/*&siteSearchFilter=e&safe=medium&q=' + query, data => callback(data.items[0].link));
+  var regEx = /^[A-Za-z0-9 _]*$/;
+  if (regEx.test(query)) {
+    $.getJSON('https://www.googleapis.com/customsearch/v1/?cx=010289098104679464911:xwek1ikjodk&key=' + googleAPIKey + '&searchType=image&num=1&siteSearch=*.facebook.com/*&siteSearchFilter=e&safe=medium&q=' + query, data => callback(data.items[0].link));
+  } else {
+    console.log('erroneous input');
+  }
 }
 
 var getLocation = function(query) {
